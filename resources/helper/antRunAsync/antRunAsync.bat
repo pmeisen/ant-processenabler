bc520bc6d085c99ef2f372ac387ae698c925047e
pmeisen <philipp@meisen.net>
2013-10-18 12:12:42 +0200

    - fixed a bug when ant was on a path with spaces

--- resources/helper/antRunAsync/antRunAsync.bat
+++ resources/helper/antRunAsync/antRunAsync.bat
@@ -1,7 +1,7 @@
 @echo off
-if exist %~dp0antRunAsync.js goto RUN_IT
+if exist "%~dp0antRunAsync.js" goto RUN_IT
 echo ERROR: %~dp0antRunAsync.js was not found!
 goto END
 :RUN_IT
-cscript //NoLogo %~dp0antRunAsync.js %1 %2 %3 %4 %5 %6 %7 %8 %9
+cscript //NoLogo "%~dp0antRunAsync.js" %1 %2 %3 %4 %5 %6 %7 %8 %9
 :END
