ca833dec3f8bda35d7931e5f637ac303c948ced7
pmeisen <philipp@meisen.net>
2014-10-06 00:39:10 +0200

    - new tests

--- test/javascriptProject/src/main.js
+++ test/javascriptProject/src/main.js
@@ -1,3 +1,15 @@
+requirejs.config({
+
+  // define the baseUrl defined by the processenabler
+  baseUrl: 'scripts',
+  
+  // add some test-function for the testcase
+  testFunction: function(a, b) { return a + (a * b); },
+  
+  // shim three.js it's not AMD conform
+  shim: { 'three': { exports: 'THREE' }}
+});
+
 require(['source'], function(src) {
   // nothing to do
 });
